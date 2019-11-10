d6f4408938c63b47cd65be5a78dc2f20181d4a32
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2019-11-10 23:09:09 +0100

    finish signup

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -2,14 +2,28 @@ import bcrypt from 'bcrypt';
 import helpers from '../helpers';
 import models from '../database/models';
 
-const { responseMessage } = helpers;
+const { responseMessage, createToken } = helpers;
 const { User } = models;
 
 export default class AuthController {
-  static signup(req, res, next) {
+  static async signup(req, res, next) {
     try {
+      // validate user existence
+      const existingUser = await User.findOne({ where: { email: req.body.email } });
+      if (existingUser) {
+        return responseMessage({
+          data: { message: 'a user with this email already exists' },
+          status: 409,
+          res
+        });
+      }
+      // create user
+      const { id } = await User.create({
+        ...req.body,
+        password: bcrypt.hashSync(req.body.password, 10)
+      });
       return responseMessage({
-        data: { message: 'yahoo yahoo' },
+        data: { message: 'done', token: createToken(id) },
         status: 200,
         res
       });

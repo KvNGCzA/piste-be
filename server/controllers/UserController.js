06f790fb85982f6309b663def4a84c0f964a1bd5
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2019-11-11 02:27:53 +0100

    finalize fetch investments

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,4 +1,3 @@
-import bcrypt from 'bcrypt';
 import helpers from '../helpers';
 import models from '../database/models';
 
@@ -6,4 +5,15 @@ const { responseMessage } = helpers;
 const { User } = models;
 
 export default class UserController {
+  static async fetchAllUserInvestments(req, res, next) {
+    try {
+      const user = await User.findByPk(req.userData.id);
+      const investments = await user.getInvestments({
+        attributes: ['id', 'name', 'amountInvested', 'expectedReturn', 'returnDate']
+      });
+      return responseMessage({ data: { investments }, status: 200, res });
+    } catch (error) {
+      next(error);
+    }
+  }
 }

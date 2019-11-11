ecaa2a5d25f43e28db0c258315de43a70fb042da
Akanmu Christopher <akanmuoluwadamilare@yahoo.com>
2019-11-11 14:28:52 +0100

    - add calculate overview function (#6)
    * - add calculate overview function
    - add investment overview route
    - add fetch all investment route
    - add pagination to all investment route
    
    * correct variable names

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,17 +1,54 @@
 import helpers from '../helpers';
 import models from '../database/models';
+import Sequelize from 'sequelize';
 
 const { responseMessage } = helpers;
 const { User, UserInvestment } = models;
 
+// [Sequelize.fn('sum', Sequelize.col('amountInvested')), 'total']
+
 export default class UserController {
+  static async calculateInvestmentOverview(req) {
+    const user = await User.findByPk(req.userData.id);
+    const investments = await user.getInvestments({
+      attributes: ['id', 'name', 'amountInvested', 'expectedReturnPercentage', 'returnDate'],
+    });
+    let totalAmountInvested = 0, totalReturnOnInvestment = 0;
+    investments.map((investment) => {
+      totalAmountInvested += investment.amountInvested ;
+      totalReturnOnInvestment += investment.amountInvested * (investment.expectedReturnPercentage/100)
+    });
+    const projectedPercentageReturn = (100/totalAmountInvested) * totalReturnOnInvestment;
+    const networth = totalAmountInvested + totalReturnOnInvestment;
+    return { numberOfInvestments: investments.length, totalAmountInvested, totalReturnOnInvestment, projectedPercentageReturn, networth };
+  }
+
+  static async myInvestmentOverview(req, res, next) {
+    try {
+      const data = await UserController.calculateInvestmentOverview(req);
+      return responseMessage({ data, status: 200, res });
+    } catch (error) {
+      next(error);
+    }
+  }
+
   static async fetchAllUserInvestments(req, res, next) {
     try {
+      const overview = await UserController.calculateInvestmentOverview(req);
       const user = await User.findByPk(req.userData.id);
+      const page = req.query.page || 1;
+      const limit = req.query.limit || 10;
+      const offset = (+page - 1) * +limit;
       const investments = await user.getInvestments({
-        attributes: ['id', 'name', 'amountInvested', 'expectedReturn', 'returnDate']
+        attributes: ['id', 'name', 'amountInvested', 'expectedReturnPercentage', 'returnDate'],
+        limit,
+        offset
       });
-      return responseMessage({ data: { investments }, status: 200, res });
+      const result = { overview, meta: { page: +page, investmentsOnPage: investments.length } };
+      if (!investments.length) {
+        return responseMessage({ data: { ...result, message: 'no additional investments to load' }, status: 400, res })
+      }
+      return responseMessage({ data: { ...result, investments }, status: 200, res });
     } catch (error) {
       next(error);
     }
@@ -25,9 +62,11 @@ export default class UserController {
         return responseMessage({ data: { message: 'this investment does not exist or does not belong to you' }, status: 400, res });
       }
       investment = await investment.getInvestment({
-        attributes: ['id', 'name', 'amountInvested', 'expectedReturn', 'returnDate']
+        attributes: ['id', 'name', 'amountInvested', 'expectedReturnPercentage', 'returnDate'],
+        raw: true
       });
-      return responseMessage({ data: { investment }, status: 200, res });
+      const totalReturnOnInvestment = investment.amountInvested * (investment.expectedReturnPercentage/100);
+      return responseMessage({ data: { ...investment, totalReturnOnInvestment }, status: 200, res });
     } catch (error) {
       next(error);
     }
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,11 +5,13 @@ import UserControllers from '../controllers/UserController';
 const user = express.Router();
 const { TokenUtils } = middlewares;
 const { verifyToken } = TokenUtils;
-const { fetchAllUserInvestments, getAnInvestment } = UserControllers;
+const { fetchAllUserInvestments, getAnInvestment, myInvestmentOverview } = UserControllers;
 
 const base = '/user';
 
 user.get(`${base}/myinvestments`, verifyToken, fetchAllUserInvestments);
+
+user.get(`${base}/myoverview`, verifyToken, myInvestmentOverview);
 
 user.get(`${base}/investment/:investmentId`, verifyToken, getAnInvestment);
 
